7d8e751a3074e2a8df682273eae8e25da330bed8
Colin Farquhar <cifarquhar@gmail.com>
2017-02-24 12:53:16 +0000

    add shelter class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ require_relative('../models/owner')
 Animal.delete_all
 Owner.delete_all
 
+shelter = Shelter.new
+
 animal1 = Animal.new({'name' => "Ringo",'admission_date' => '2017-02-23 15:00:00', 'adoptable' => true, 'training_status' => false, 'vet_status' => false})
 animal2 = Animal.new({'name' => "Fudge",'admission_date' => '2016-12-10 16:00:00', 'adoptable' => false, 'training_status' => true, 'vet_status' => true})
 
---
+++ models/shelter.rb
@@ -0,0 +1,11 @@
+require_relative('animal')
+require_relative('owner')
+
+class Shelter
+
+  def initialize
+  end
+
+
+
+end
