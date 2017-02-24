f46a25f1adebf246afe4382b7c14442666246de8
Colin Farquhar <cifarquhar@gmail.com>
2017-02-24 13:18:24 +0000

    add new_animal method to shelter class

--- models/shelter.rb
+++ models/shelter.rb
@@ -14,4 +14,11 @@ class Shelter
     Owner.all()
   end
 
+  def new_animal(animal_name,animal_adoptable,animal_training_status,animal_vet_status)
+    timestamp = Time.new.to_s
+    trimmed_timestamp = 6.times do timestamp.chop! end
+    new_animal = Animal.new({'name' => "#{animal_name}", 'admission_date' => timestamp, 'adoptable' => animal_adoptable, 'training_status' => animal_training_status, 'vet_status' => animal_vet_status})
+    new_animal.save()
+  end
+
 end
