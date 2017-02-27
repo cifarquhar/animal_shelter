3220f1943fb1a9cba14fc144e1db632408c2712b
Colin Farquhar <cifarquhar@gmail.com>
2017-02-27 11:22:42 +0000

    clean up shelter class

--- models/shelter.rb
+++ models/shelter.rb
@@ -6,13 +6,6 @@ class Shelter
   def initialize()
   end
 
-  # def list_animals()
-  #   Animal.all()
-  # end
-
-  # def list_owners()
-  #   Owner.all()
-  # end
 
   def new_animal(animal_name,animal_type,animal_training_status,animal_vet_status)
     timestamp = Time.new.to_s
@@ -25,14 +18,6 @@ class Shelter
     new_animal.save()
   end
 
-  # def new_owner(owner_name)
-  #   new_owner = Owner.new({'name' => "#{owner_name}"})
-  #   new_owner.save()
-  # end
-
-  # def assign_animal(owner,animal)
-  #   owner.adopt_animal(animal) if animal.adoptable == true
-  # end
 
   def check_adoptable_status(animal)
     if animal.vet_status == false && animal.training_status == false
