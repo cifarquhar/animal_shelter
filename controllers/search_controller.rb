023a12de88fa45e49d3057b42386d750827fd5af
Colin Farquhar <cifarquhar@gmail.com>
2017-02-28 09:30:24 +0000

    add search by type function

--- controllers/search_controller.rb
+++ controllers/search_controller.rb
@@ -12,7 +12,13 @@ end
 
 post '/search' do
   name_to_search = params[:name].downcase
-  animals = Animal.all
-  searched_animal = animals.find {|animal| animal.name.downcase == name_to_search}
-  redirect to "animals/#{searched_animal.id}"
+  type_to_search = params[:type].downcase
+  animals_to_search = Animal.all
+  if name_to_search != ""
+    searched_animal = animals_to_search.find {|animal| animal.name.downcase == name_to_search}
+    redirect to "animals/#{searched_animal.id}"
+  else
+    @animals = animals_to_search.find_all {|animal| animal.type.downcase == type_to_search}
+    erb(:"animals/index")
+  end
 end
